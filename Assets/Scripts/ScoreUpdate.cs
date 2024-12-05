57ad301622bbaf4ffae61f86a93de37f91315c8c
oD Wreckz <114649822+Nikon-ref@users.noreply.github.com>
2024-12-05 18:02:36 +0000

    EditorAplication.EditPlaymode
    changed into If statement with Application.Quit

--- Assets/Scripts/ScoreUpdate.cs
+++ Assets/Scripts/ScoreUpdate.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using UnityEditor;
 using UnityEngine;
 
 public class ScoreUpdate : MonoBehaviour
@@ -14,13 +13,13 @@ public class ScoreUpdate : MonoBehaviour
         if (coinParent.childCount <= 0)
         {
             Debug.Log("You Lose! All coins have been collected.");
-            EditorApplication.ExitPlaymode();
+            EndGame();
         }
         // Check if all enemies are dead
         else if (enemyParent.childCount <= 0)
         {
             Debug.Log("You Win! All enemies are dead.");
-            EditorApplication.ExitPlaymode();
+            EndGame();
         }
         else
         {
@@ -28,4 +27,44 @@ public class ScoreUpdate : MonoBehaviour
             Debug.Log("Coins Collected by the enemies: " + collected);
         }
     }
+
+    void EndGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
 }
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor;
+//using UnityEngine;
+
+//public class ScoreUpdate : MonoBehaviour
+//{
+//    public int collected = 0;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//
+//    }
+
+// Update is called once per frame
+//    void Update()
+//    {
+//        //if statement to display on the console that if the prefab count from the spawner is 0 or less you will win the game and exit playmode
+//        if (this.transform.childCount <= 0)
+//        {
+//            Debug.Log("You Win!");
+//            EditorApplication.ExitPlaymode();
+//        }
+//        else
+//        {
+//            //otherwise it will display how many coins you have collected so far
+//           Debug.Log("Coins Collected: " + collected);
+//        }
+//    }
+//}
